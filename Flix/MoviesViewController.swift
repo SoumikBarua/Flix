788989f2537b9b864880f95d0f57471378c6c88d
Soumik Barua <10300902+SoumikBarua@users.noreply.github.com>
2020-09-25 23:10:55 -0400

    Add AlamofireImage library using Cocoapods, display movie posters

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -62,6 +63,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterURL!)
         
         return cell
     }
