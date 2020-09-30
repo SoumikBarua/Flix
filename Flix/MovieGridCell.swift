d17dfe21f6731b2ad0169f0c84b96e4316b45124
Soumik Barua <10300902+SoumikBarua@users.noreply.github.com>
2020-09-30 15:22:59 -0400

    Add collection view, set up its cell & layout

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by SB on 9/30/20.
+//  Copyright © 2020 SB. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
